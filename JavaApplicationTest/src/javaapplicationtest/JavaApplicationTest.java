bc1a46de9bf6121ac2ef0654175a86c595453257
Asier <abilbaotm@gmail.com>
2017-05-16 21:02:07 +0200

    fallo

--- JavaApplicationTest/src/javaapplicationtest/JavaApplicationTest.java
+++ JavaApplicationTest/src/javaapplicationtest/JavaApplicationTest.java
@@ -15,7 +15,7 @@ public class JavaApplicationTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Test travis-ci");
+        System.out.prln("Test travis-ci");
     }
     
 }
